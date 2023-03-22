81df5e22a0913513b1c14cc8a71dc1b1d7d1e417
Daniel Vaxman <77477191+Dvaxinator@users.noreply.github.com>
2023-03-21 22:50:53 -0400

    Finished The Bottom Navigation Bar

--- app/src/main/java/com/example/grocerybuddy/UserMenu.java
+++ app/src/main/java/com/example/grocerybuddy/UserMenu.java
@@ -1,31 +1,61 @@
 package com.example.grocerybuddy;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.example.grocerybuddy.databinding.ActivityUserMenuBinding;
+
 public class UserMenu extends AppCompatActivity {
 
+    ActivityUserMenuBinding binding;
     TextView welcomeMsg;
-    Button btn;
-    int counter = 0;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_menu);
+        replaceFragment(new MapFragment());
+        binding = ActivityUserMenuBinding.inflate(getLayoutInflater());
+        binding.bottomNavigationView.getMenu().findItem(R.id.map).setChecked(true);
+        setContentView(binding.getRoot());
 
+        //setContentView(R.layout.activity_user_menu);
         welcomeMsg = findViewById(R.id.welcomeMessage);
-        btn = findViewById(R.id.button);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                
-            }
-        });
 
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            switch (item.getItemId()) {
+                case R.id.discounts:
+                    replaceFragment(new DiscountFragment());
+                    break;
+                case R.id.list:
+                    replaceFragment(new ListFragment());
+                    break;
+                case R.id.map:
+                    replaceFragment(new MapFragment());
+                    break;
+                case R.id.account:
+                    replaceFragment(new AccountFragment());
+                    break;
+                case R.id.settings:
+                    replaceFragment(new SettingsFragment());
+                    break;
+
+
+            }
+
+            return true;
+        });
+    }
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
